b238f2cf4d476d7da76804cadc2d6000fd774d49
WebCasting28 <ansarisikkim786@gmail.com>
2025-10-06 21:22:32 +0530

    employee part complete

--- employee-payroll-system/src/main/java/com/mycompany/app/App.java
+++ employee-payroll-system/src/main/java/com/mycompany/app/App.java
@@ -1,10 +1,100 @@
 package com.mycompany.app;
 
-/**
- * Hello world!
- */
+import com.mycompany.app.dao.*;
+import com.mycompany.app.model.Employee;
+import com.mycompany.app.model.EmployeeType;
+
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        EmployeeDAO employeeDAO = new EmployeeDAO();
+        Scanner sc = new Scanner(System.in);
+        boolean flag = true;
+
+        while (flag) {
+            System.out.println("Select options");
+            System.out.println("1. Add new employee");
+            System.out.println("2. Add employee salary");
+            System.out.println("3. Exit");
+            int option = sc.nextInt();
+            sc.nextLine();
+
+            switch (option) {
+                case 1:
+                    boolean innerflag = true;
+                    while (innerflag) {
+                        System.out.println("\n--- Employee Management ---");
+                        System.out.println("1. Add Employee");
+                        System.out.println("2. View All Employees");
+                        System.out.println("3. Update Employee Details");
+                        System.out.println("4. Delete Employee");
+                        System.out.println("5. View Employee by ID");
+                        System.out.println("6. Back to Main Menu");
+                        System.out.print("Enter your choice: ");
+                        int empOption = sc.nextInt();
+                        sc.nextLine();
+
+                        switch (empOption) {
+                            case 1:
+                                System.out.print("Enter Employee Name : ");
+                                String emp_name = sc.nextLine();
+                                System.out.print("Enter Employee Type (FULLTIME / PARTTIME ): ");
+                                String emptype = sc.nextLine().toUpperCase();
+                                EmployeeType emp_type = EmployeeType.valueOf(emptype);
+                                Employee add_employee = new Employee(emp_name, emp_type);
+                                employeeDAO.emp_add(add_employee);
+
+                                break;
+                            case 2:
+                                employeeDAO.view_all_emp();
+                                break;
+                            case 3:
+                                System.out.print("Enter Employee ID : ");
+                                int emp_id = sc.nextInt();
+                                sc.nextLine();
+                                System.out.print("Enter Employee Name : ");
+                                String update_emp_name = sc.nextLine();
+                                System.out.print("Enter Employee Type (FULLTIME / PARTTIME ) : ");
+                                String updateemptype = sc.nextLine().toUpperCase();
+                                EmployeeType update_emp_type = EmployeeType.valueOf(updateemptype);
+                                Employee update_employee = new Employee(emp_id, update_emp_name, update_emp_type);
+                                employeeDAO.emp_update(update_employee);
+                                break;
+                            case 4:
+                                System.out.print("Enter Employee ID : ");
+                                int delete_emp_id = sc.nextInt();
+                                employeeDAO.emp_delete(delete_emp_id);
+                                break;
+                            case 5:
+                                System.out.print("Enter Employee ID : ");
+                                int view_emp_id = sc.nextInt();
+                                Employee employee_details = employeeDAO.emp_details(view_emp_id);
+                                System.out.println(employee_details);
+                                break;
+                            case 6:
+                                innerflag = false;
+                                break;
+
+                            default:
+                                System.out.println("Invalid option");
+                                break;
+                        }
+                    }
+
+                    break;
+                case 2:
+                    System.out.println("Working progress");
+                    break;
+                case 3:
+                    flag = false;
+                    System.out.println("Exiting program...");
+                    break;
+                default:
+                    System.out.println("Invalid option");
+                    break;
+            }
+
+        }
     }
 }
--- employee-payroll-system/src/main/java/com/mycompany/app/dao/EmployeeDAO.java
+++ employee-payroll-system/src/main/java/com/mycompany/app/dao/EmployeeDAO.java
@@ -11,16 +11,101 @@ import com.mycompany.app.service.PayrollService;
 
 public class EmployeeDAO {
 
-    public void emp_add(String name, EmployeeType type){
+    /* Employee details APIs */
+
+    public void emp_add(Employee employee) {
         String sql = "INSERT INTO emp_details (emp_name, emp_type) VALUES(?,?)";
-        try(Connection con = DBConnection.getConnection(); PreparedStatement ps = con.prepareStatement(sql)) {
-             ps.setString(1, name);
-             ps.setString(2, type.name());
-             ps.executeUpdate();
-             System.out.println("Employee added successfully");
+        try (Connection con = DBConnection.getConnection(); PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setString(1, employee.get_emp_name());
+            ps.setString(2, employee.get_emp_type().name());
+            ps.executeUpdate();
+            System.out.println("Employee added successfully");
         } catch (Exception e) {
             System.out.println("Server error");
             e.printStackTrace();
         }
+    }
+
+    public void emp_delete(int id) {
+        String sql1 = "DELETE FROM emp_details WHERE emp_id=?";
+        String sql2 = "DELETE FROM emp_payroll WHERE emp_id=?";
+        try (Connection con = DBConnection.getConnection();
+                PreparedStatement ps1 = con.prepareStatement(sql1)
+                /*PreparedStatement ps2 = con.prepareStatement(sql2)*/) {
+            ps1.setInt(1, id);
+            // ps2.setInt(2, id);
+            ps1.executeUpdate();
+            // ps2.executeUpdate();
+            System.out.println("employee details delete");
+
+        } catch (Exception e) {
+            System.out.println("Server error");
+            e.printStackTrace();
+        }
+    }
+
+    public void emp_update(Employee employee) {
+        String sql = "UPDATE emp_details SET emp_name=?, emp_type=? WHERE emp_id=?";
+        try (Connection con = DBConnection.getConnection(); PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setString(1, employee.get_emp_name());
+            ps.setString(2, employee.get_emp_type().name());
+            ps.setInt(3, employee.get_emp_id());
+            ps.executeUpdate();
+            System.out.println("employee details updated");
+        } catch (Exception e) {
+            System.out.println("Server error");
+            e.printStackTrace();
+        }
+
+    }
+
+    public Employee emp_details(int id) {
+        String sql = "SELECT * FROM emp_details WHERE emp_id=?";
+        try (Connection con = DBConnection.getConnection(); PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setInt(1, id);
+            try (ResultSet res = ps.executeQuery()) {
+                if (res.next()) {
+                   
+                    return new Employee(
+                            res.getInt("emp_id"),
+                            res.getString("emp_name"),
+                            EmployeeType.valueOf(res.getString("emp_type"))
+
+                    );
+                }
+            }
+
+        } catch (Exception e) {
+            System.out.println("Server error");
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void view_all_emp() {
+        String sql = "SELECT * FROM emp_details";
+        try (Connection con = DBConnection.getConnection();
+                Statement stmt = con.createStatement();
+                ResultSet rs = stmt.executeQuery(sql)) {
+                    while(rs.next()){
+                        Employee employee = new Employee(
+                         rs.getInt("emp_id"),
+                         rs.getString("emp_name"),
+                         EmployeeType.valueOf(rs.getString("emp_type"))
+                         );
+                         System.out.println(employee);
+                    }
+
+        } catch (Exception e) {
+            System.out.println("Server error");
+            e.printStackTrace();
+        }
+
+    }
+
+    /* Employee Payroll APIs */
+
+    public void add_salary(){
+        
     }
 }
